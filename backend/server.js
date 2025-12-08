3d7b5436e508d30d2a75c10ee82588b400cdc07e
Tobe Marizu <tobemarizu@gmail.com>
2025-12-08 15:36:26 +0100

    clerk me

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,22 @@ import userRoutes from './routes/user.routes.js';
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+ await connectDB()
 await connectCloudinary();
+
 app.use(clerkMiddleware());// Clerk middleware to handle authentication
 
 app.use(requireAuth())// Protect all routes below this middleware
-app.use(express.json());// To parse JSON request bodies
 app.get('/', (req, res)=>res.send('API is running...'));
+app.use(express.json());// To parse JSON request bodies
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+
+//port
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
-    connectDB();
 })
